a3f46c1261a48ecefd9fcb24e52d2dc9b4f60e54
Sandip Thapa <sandipthapa386@gmail.com>
2021-06-18 20:57:11 +0545

    Day 110

---
+++ 110-Adding Words.py
@@ -0,0 +1,11 @@
+def concat(*args):
+    result = '-'.join(args)
+    print (result)
+    
+concat("Hello", "Conections", "Greetings", "!")
+
+# =======
+# Result:
+# ================================================
+# Hello-Conections-Greetings-!
+# ================================================
