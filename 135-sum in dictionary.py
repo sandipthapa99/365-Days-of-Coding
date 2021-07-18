2d160ebcac416e7505fa3af0ee3b5899057e868a
Sandip Thapa <sandipthapa386@gmail.com>
2021-07-18 19:42:07 +0545

    Day 135

---
+++ 135-sum in dictionary.py
@@ -0,0 +1,12 @@
+# sum function in dictionary
+dict = {1:2,3:4}
+print(sum(dict))
+
+# On passing dictionary as the argument of sum function
+# it will return the sum of keys if they are integers
+
+# --------
+# Result:
+# --------------------------------------------------------
+# 4
+# --------------------------------------------------------
