b4ad63131f35c5b0a5b65227472a0edf30786528
Sandip Thapa <sandipthapa386@gmail.com>
2021-07-18 09:06:57 +0545

    Day 134

---
+++ 134.py
@@ -0,0 +1,14 @@
+# list append
+
+my_list = [2,4,6]
+my_list.append(5,4)
+print(list)
+
+# above code produce error as the append() function
+# only takes one argument but we passed 2
+
+# --------
+# Result:
+# --------------------------------------------------------
+# TypeError: append() takes exactly one argument (2 given)
+# --------------------------------------------------------
