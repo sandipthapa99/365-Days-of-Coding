459f64414fe273e7484d15fcccd4843ec308aa53
Sandip Thapa <sandipthapa386@gmail.com>
2021-07-28 18:29:38 +0545

    Day 148

--- 148-text to speech.py
+++ 148-text to speech.py
@@ -1,4 +1,4 @@
 import pyttsx3
-engine = pyttsx3.init()
-engine.say("I love Python Programming!")
-engine.runAndWait()
+reader = pyttsx3.init()
+reader.say("I love Python Programming!")
+reader.runAndWait()
