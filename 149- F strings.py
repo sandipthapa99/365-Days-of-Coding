29e15ab6c061ad4f18bc3a99b302c6bb4392a603
Sandip Thapa <sandipthapa386@gmail.com>
2021-07-29 16:41:36 +0545

    Day 149

---
+++ 149- F strings.py
@@ -0,0 +1,15 @@
+x = input("Show name:")
+y = int(input("Total seasons:"))
+
+# ======================================
+print(x + " has " + str(y) + " Seasons.")
+# ==================VS==================
+print(f"{x} has {y} seasons.")
+# ======================================
+# Input:
+# Rick & Morty
+# 4
+# # ======================================
+# Output:
+# Rick & Morty has 4 Seasons.
+# Rick & Morty has 4 Seasons.
