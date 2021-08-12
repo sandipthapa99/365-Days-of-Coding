3353ed861c572b29d23c81bd1e2b1c2581ff617c
Sandip Thapa <sandipthapa386@gmail.com>
2021-08-12 19:16:21 +0545

    Day 162

---
+++ 162-break vs continue.py
@@ -0,0 +1,14 @@
+# Break                     # --------
+for num in range(4):        # Result:
+    if num == 2:            # --------
+        break               # 0
+    print(num)              # 1
+                            # -------
+
+# Continue                  # --------
+for num in range(4):        # Result:
+    if num == 2:            # --------
+        continue            # 0
+    print(num)              # 1
+                            # 2
+                            # --------
