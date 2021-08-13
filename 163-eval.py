ae50981699bce601f5f3885be0789a4e1a71efb8
Sandip Thapa <sandipthapa386@gmail.com>
2021-08-13 17:21:16 +0545

    Day 163

---
+++ 163-eval.py
@@ -0,0 +1,21 @@
+expression1 = "3+5"
+expression2 = "2+7-5"
+expression3 = "3*7+2"
+expression4 = "12/6+7*8"
+expression5 = "67/4*4+6-2"
+
+print(eval(expression1))
+print(eval(expression2))
+print(eval(expression3))
+print(eval(expression4))
+print(eval(expression5))
+
+# --------
+# Result:
+# ------------------------
+# 8
+# 4   
+# 23  
+# 58.0
+# 71.0
+# ------------------------
